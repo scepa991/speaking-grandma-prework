dda0466efb7105205bc9957ca73028a214c8a2e2
Stefan Scepanovic <scepa91@icloud.com>
2019-05-10 00:18:05 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,8 +1,10 @@
 def speak_to_grandma(phrase)
 
-  if phrase == "I LOVE YOU GRANDMA!"
-    return "I LOVE YOU TO PUMPKIN!"
-
+  if phrase != phrase.upcase()
+    return "HUH?! SPEAK UP, SONNY!"
+  else
+    return "NO, NOT SINCE 1938!"
+end
 
 
 
